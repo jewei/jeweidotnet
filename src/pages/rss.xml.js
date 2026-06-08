1f46bd4055b6cf93c0dbda28fa6e6785c5bc7912
Jewei Mak <jewei.mak@gmail.com>
2026-06-08 09:12:24 +0800

    refactor: reorganize utility imports from 'utils' to 'support'

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,6 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-import { postUrl } from '../utils/blog';
+import { postUrl } from '../support/blog';
 
 export async function GET(context) {
   const posts = (await getCollection('blog', ({ data }) => !data.draft))
